57eb76cd073b1ebb0bb4377c36907cbdaddc32d0
mdmonjumon <mdmonjumon786@gmail.com>
2025-02-09 18:04:05 +0600

    react project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-purple-500 font-extrabold text-5xl '>Knowledge Cafe</h1>
+
+    </>
+  )
+}
+
+export default App
